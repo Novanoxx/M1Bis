4f83c126432567552679c308201da69af0961b9d
stephane.vong <NovanoxCorp@outlook.fr>
2022-10-13 13:31:35 +0200

    tp3 java fini

--- Java/TP3/src/main/java/fr/uge/slice/Slice3.java
+++ Java/TP3/src/main/java/fr/uge/slice/Slice3.java
@@ -2,8 +2,29 @@ package fr.uge.slice;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 interface Slice3<E> {
+    default Slice3<E> subSlice(int from, int to) {
+        Objects.checkFromToIndex(from, to, size());
+        return new Slice3<>() {
+            @Override
+            public int size() {
+                return to - from;
+            }
+
+            @Override
+            public E get(int index) {
+                Objects.checkIndex(index, size());
+                return Slice3.this.get(index + from);
+            }
+
+            @Override
+            public String toString() {
+                return Arrays.toString(IntStream.range(0, size()).mapToObj(this::get).toArray());
+            }
+        };
+    }
     static <E> Slice3<E> array(E[] array) {
         Objects.requireNonNull(array);
         return new Slice3<>() {
@@ -12,11 +33,6 @@ interface Slice3<E> {
             }
             public E get(int index) {
                 return array[index];
-            }
-            @Override
-            public Slice3<E> subSlice(int from, int to) {
-                Objects.checkFromToIndex(from, to, array.length);
-                return Slice3.array(array, from, to);
             }
             public String toString() {
                 return Arrays.toString(Arrays.stream(array).toArray());
@@ -27,31 +43,8 @@ interface Slice3<E> {
     static <E> Slice3<E> array(E[] array, int from, int to) {
         Objects.requireNonNull(array);
         Objects.checkFromToIndex(from, to, array.length);
-        return new Slice3<>() {
-            @Override
-            public int size() {
-                return to - from;
-            }
-            @Override
-            public E get(int index) {
-                Objects.checkIndex(index, to - from) ;
-                return array[from + index];
-            }
-            @Override
-            public Slice3<E> subSlice(int from2, int to2) {
-                Objects.checkFromToIndex(from2, to2, array.length);
-                if (size() < to2 - from2) {
-                    throw new IndexOutOfBoundsException();
-                }
-                return Slice3.array(array, from + from2, to + from2);
-            }
-            @Override
-            public String toString() {
-                return Arrays.toString(Arrays.stream(array, from, to).toArray());
-            }
-        };
+        return array(array).subSlice(from, to);
     }
     int size();
     E get(int index);
-    Slice3<E> subSlice(int from, int to);
 }
