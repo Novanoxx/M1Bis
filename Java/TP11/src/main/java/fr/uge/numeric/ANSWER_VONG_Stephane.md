c6c1c9d215f8fc89e354aadd2ad3ee291f662900
stephane.vong <NovanoxCorp@outlook.fr>
2022-12-09 15:44:59 +0100

    tp11 java

---
+++ Java/TP11/src/main/java/fr/uge/numeric/ANSWER_VONG_Stephane.md
@@ -0,0 +1,49 @@
+public class NumericVec<E> {
+private int size;
+private int capacity;
+private long[] interArray;
+
+    private NumericVec() {
+        this.size = 0;
+        this.capacity = 16;
+        this.interArray = new long[capacity];
+    }
+
+    public static NumericVec<Long> longs() {
+        return new NumericVec<>();
+    }
+
+    public static NumericVec<Integer> ints() {
+        return new NumericVec<>();
+    }
+
+    public static NumericVec<Double> doubles() {
+        return new NumericVec<>();
+    }
+
+    public void add(E value) {
+        Objects.requireNonNull(value);
+        if (size == interArray.length) {
+            this.capacity *= 2;
+            interArray = Arrays.copyOf(interArray, capacity);
+        }
+        this.interArray[size()] = (long) value;
+        this.size++;
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size());
+        return this.interArray[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(interArray).filter(Objects::nonNull).map(Object::toString).collect(Collectors.joining(", ", "[", "]"));
+    }
+
+
+}
---
+++ Java/TP11/src/main/java/fr/uge/numeric/NumericVec.java
@@ -0,0 +1,101 @@
+package fr.uge.numeric;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class NumericVec<E> implements Iterable<E>{
+    private int size;
+    private int capacity;
+    private long[] interArray;
+    private final Function<E, Long> into;
+    private final Function<Long, E> from;
+
+    private NumericVec() {
+        this.size = 0;
+        this.capacity = 16;
+        this.into = v -> (Long) v;
+        this.from = v -> (E) v;
+        this.interArray = new long[capacity];
+    }
+    private NumericVec(Function<E, Long> into, Function<Long, E> from) {
+        this.size = 0;
+        this.capacity = 16;
+        this.into = into;
+        this.from = from;
+        this.interArray = new long[capacity];
+    }
+
+    public static NumericVec<Long> longs(long... values) {
+        var numVec = new NumericVec<>(i -> i, i -> i);
+        for (var value : values) {
+            numVec.add(value);
+        }
+        return numVec;
+    }
+
+    public static NumericVec<Integer> ints(int... values) {
+        var numVec = new NumericVec<>(i -> (long) i, i -> Integer.parseInt(String.valueOf(i)));
+        for (var value : values) {
+            numVec.add(value);
+        }
+        return numVec;
+    }
+
+    public static NumericVec<Double> doubles(double... values) {
+        var numVec = new NumericVec<>(Double::doubleToRawLongBits, Double::longBitsToDouble);
+        for (var value : values) {
+            numVec.add(value);
+        }
+        return numVec;
+    }
+
+    public void add(E value) {
+        Objects.requireNonNull(value);
+        if (size == interArray.length) {
+            this.capacity *= 2;
+            interArray = Arrays.copyOf(interArray, capacity);
+        }
+        this.interArray[size()] = into.apply(value);
+        this.size++;
+    }
+
+    public E get(int index) {
+        Objects.checkIndex(index, size());
+        return from.apply(this.interArray[index]);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.stream(interArray).limit(size).filter(Objects::nonNull).boxed().map(e -> from.apply(e).toString()).collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int it = 0;
+            private final int sizeIt = size;
+            @Override
+            public boolean hasNext() {
+                return it < sizeIt;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("No next value");
+                }
+                var current = it;
+                it++;
+                return get(current);
+            }
+        };
+    }
+}
