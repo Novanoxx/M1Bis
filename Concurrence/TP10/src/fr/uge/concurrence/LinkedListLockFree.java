5a7c86e92a4b6cb3fec489b6a04c05c5b513f7c2
Novanox <NovanoxCorp@outlook.fr>
2022-12-13 21:29:24 +0100

    tp10 concu finished

--- Concurrence/TP10/src/fr/uge/concurrence/LinkedListLockFree.java
+++ Concurrence/TP10/src/fr/uge/concurrence/LinkedListLockFree.java
@@ -1,5 +1,6 @@
 package fr.uge.concurrence;
 
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
@@ -15,12 +16,32 @@ public class LinkedListLockFree<E> {
 
     public void addFirst(E value) {
         Objects.requireNonNull(value);
-        var current = head.get();
-
+        for (;;) {
+            var current = head.get();   // volatile read
+            if (current == null) {
+                head.set(new Link<>(value, null));  // volatile write
+                return;
+            }
+            if (head.compareAndSet(current, new Link<>(value, current))) {    // volatile read/write
+                return;
+            }
+        }
     }
 
     public void forEach(Consumer<? super E> consumer) {
+        Objects.requireNonNull(consumer);
+        for (var current = head.get(); current != null; current = current.next) {
+            consumer.accept(current.value());
+        }
+    }
 
+    public E pollFirst() {
+        var current = head.get();
+        if (current == null) {
+            return null;
+        }
+        head.updateAndGet(e -> e.next);
+        return current.value();
     }
 
     public static void main(String[] args) {
@@ -28,6 +49,9 @@ public class LinkedListLockFree<E> {
         list.addFirst("Noel");
         list.addFirst("papa");
         list.addFirst("petit");
+        list.forEach(System.out::println);
+
+        System.out.println("poll : " + list.pollFirst());
         list.forEach(System.out::println);
     }
 }
